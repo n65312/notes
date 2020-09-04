ca62c79a7e1ace2e32c5343b427019d5c43e29ae
Nathan <n17821177949@outlook.com>
2020-09-05 04:14:55 +0800

    feat:修改config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,12 +8,57 @@ module.exports = {
   dest: 'dist',
   title: 'Lin\'s Blog',
   description: '记录，分享',
-  head,
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  ],
   themeConfig: {
-    // smoothScroll: true,
-    lastUpdated: '上次更新', // string | boolean
-    sidebar,
-    nav
+    smoothScroll: true,
+    lastUpdated: '上次更新',
+    nav: [
+      { text: '首页', link: '/' },
+      {
+        text: '前端',
+        items: [
+          { text: 'JavaScript', link: '/js/' },
+          { text: 'CSS', link: '/css/' },
+        ],
+      },
+      { text: '计算机', link: '/computer' },
+      { text: 'Github', link: 'https://github.com/n65312/notes' },
+    ],
+    sidebar: {
+      '/css/': [
+        '',
+        'css1',
+        'css2',
+        'css3',
+      ],
+      '/js/': [
+        '',
+        'js1',
+        'js2',
+      ],
+      '/': [
+        'computer',
+      ]
+    }
   },
-  plugins
+  plugins: {
+    '@vuepress/pwa': {
+      serviceWorker: true,
+      updatePopup: {
+        message: "发现新内容可用",
+        buttonText: "刷新"
+      }
+    },
+    '@vuepress/back-to-top': true
+  }
 }
--- docs/.vuepress/config/secret.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  clientId: 'ee892c00c68d07e043e3',
-  clientSecret: '3232c2dcc8285b363252475cb3aaa16c4e938a67',
-  ga: 'UA-176834893-1' 
-}
